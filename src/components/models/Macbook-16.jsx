006b7029da894f4f7c334f157fb1b2f1e5e01248
salehAbdessalem <salih.abdessalem@gmail.com>
2025-10-19 13:01:26 +0100

    Fix material override

--- src/components/models/Macbook-16.jsx
+++ src/components/models/Macbook-16.jsx
@@ -123,7 +123,6 @@ export default function MacbookModel16(props) {
       />
       <mesh
         geometry={nodes.Object_123.geometry}
-        material={materials.sfCQkHOWyrsLmor}
         rotation={[Math.PI / 2, 0, 0]}
       >
         <meshBasicMaterial map={texture} />
